cb8054fc27d3198ed64054a20242b107999e4a08
SK <trivedisaksham@gmail.com>
2024-04-02 18:33:11 +0530

    Testing Changes

--- SendLogs/ReadLogs.py
+++ SendLogs/ReadLogs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 from datetime import datetime
+import re as read_lines
 
 ############################################## READIN A LOG FILE SECTION ###########################################################
 
@@ -10,5 +11,29 @@ log_dir = 'daily-logs'
 
 logs = open(log_dir + '/' + log_file)
 
+
 for log in (logs):
-    print(log.readline( ))
+ #  print(log.strip())
+
+    # Sample text containing the output
+
+    text = log
+
+    # Regular expression to find percentage values
+#   pattern = r'(\d+)%'
+
+#   # Find all percentage values in the text
+#   percentages = read_lines.findall(pattern, text)
+
+#   # Print the extracted percentage values
+#   for percentage in percentages:
+#       print(f"Percentage: {percentage}%")
+    # Regular expression to find lines containing '85% '
+    pattern = r'.*85%\s.*'
+
+    # Find all lines containing '85% '
+    lines_with_85_percent = read_lines.findall(pattern, text)
+
+    # Print the lines containing '85% '
+    for line in lines_with_85_percent:
+        print(line)
