ba69df0cd4225dd6c80fc18caa96b7f8d5c98dcf
SK <trivedisaksham@gmail.com>
2026-02-01 18:36:49 +0530

    Implementation functions for Caesar Cipher

---
+++ Ceaser-Ciphers/Decryption.py
@@ -0,0 +1,18 @@
+def decrypt_caesar(text, shift):
+    result = ""
+
+    for char in text:
+        if char.isupper():
+            base = ord('A')
+            new_char = chr((ord(char) - base - shift) % 26 + base)
+            result += new_char
+
+        elif char.islower():
+            base = ord('a')
+            new_char = chr((ord(char) - base - shift) % 26 + base)
+            result += new_char
+
+        else:
+            result += char
+
+    return result
